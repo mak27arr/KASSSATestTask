f9e664eb6c5f0f29d446f8a0f609fe359af1ad32
makar <mak27arr@gmail.com>
2020-09-02 17:49:05 +0300

    swager add

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Ninject;
 using Ninject.Modules;
 using System;
@@ -21,14 +22,18 @@ namespace KASSSATestTask
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public IServiceProvider ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            string defConString = Configuration.GetConnectionString("DefaultConnection");
-            NinjectModule objectiveModule = new ObjectiveModule();
-            NinjectModule serviceModule = new ServiceModule(defConString);
-            IKernel kernel = new StandardKernel(objectiveModule, serviceModule);
-            return kernel.Get<IServiceProvider>();
+            //string defConString = Configuration.GetConnectionString("DefaultConnection");
+            //NinjectModule objectiveModule = new ObjectiveModule();
+            //NinjectModule serviceModule = new ServiceModule(defConString);
+            //IKernel kernel = new StandardKernel(objectiveModule, serviceModule);
+            //return kernel.Get<IServiceProvider>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Objective API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,7 +46,11 @@ namespace KASSSATestTask
             app.UseHttpsRedirection();
             app.UseRouting();
             //app.UseAuthorization();
-            
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Objective API V1");
+            });
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
